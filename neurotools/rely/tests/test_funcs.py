0cf13ec21e57702ed7d81718e7b7e31f91f3c269
sahahn <sahahn@uvm.edu>
2021-09-29 15:41:49 -0400

    Add extra test

--- neurotools/rely/tests/test_funcs.py
+++ neurotools/rely/tests/test_funcs.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 import pandas as pd
 from ...loading.funcs import load
-from ..funcs import run_rely
+from ..funcs import run_rely, _test_split
 
 file_dr = os.path.dirname(os.path.realpath(__file__))
 
@@ -43,3 +43,37 @@ def test_rely_basic_data_df():
     run_rely(covars_df,
              data_df=data_df,
              max_size=30, verbose=-1)
+
+def test_test_split_groups():
+
+    all_subjects = ['s1', 's2', 's3', 's4', 's5']
+    stratify = None
+    groups = pd.Series([1, 1, 2, 2, 3], index=all_subjects)
+
+    for split in range(50):
+
+        g1, g2 = _test_split(all_subjects, stratify,
+                             groups, split_random_state=split)
+
+        if 's1' in g1:
+            assert 's2' in g1
+        elif 's1' in g2:
+            assert 's2' in g2
+
+        if 's3' in g1:
+            assert 's4' in g1
+        elif 's3' in g2:
+            assert 's4' in g2
+
+
+def test_test_split_stratify():
+
+    all_subjects = ['s1', 's2', 's3', 's4', 's5', 's6', 's7', 's8']
+    stratify = pd.Series([1, 1, 1, 1, 0, 0, 0, 0], index=all_subjects)
+    groups = None
+
+    for split in range(50):
+
+        g1, g2 = _test_split(all_subjects, stratify,
+                             groups, split_random_state=split)
+        assert np.sum(stratify.loc[g1]) == np.sum(stratify.loc[g2])
