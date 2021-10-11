d214a72347d3753ced4fc957e6b84fc82a4e65ef
sahahn <sahahn@uvm.edu>
2021-10-11 13:55:06 -0400

    .1 looks bad

--- src_doc/source/conf.py
+++ src_doc/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Sage Hahn'
 author = 'Sage Hahn'
 
 # The full version, including alpha/beta/rc tags
-release = '.1'
+release = '0.1'
 
 
 # -- General configuration ---------------------------------------------------
