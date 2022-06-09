a17be50040be61822122ce85f832ad754b533f95
sahahn <sahahn@uvm.edu>
2022-06-09 11:20:42 -0400

    fix

--- neurotools/transform/rois.py
+++ neurotools/transform/rois.py
@@ -377,6 +377,25 @@ class SurfLabels(BaseEstimator, TransformerMixin):
             # Data dim is 0 since the shapes match
             self.data_dim_ = 0
 
+    @property
+    def roi_labels(self):
+        return {i: label for i, label in enumerate(self.non_bkg_unique_)}
+
+    def labels_to_roi_index(self, labels):
+
+        label_map = {label: i for i, label in enumerate(self.non_bkg_unique_)}
+        roi_index = np.array([label_map[l] for l in labels])
+        
+        return roi_index
+
+    def locs_to_roi_index(self, locs):
+        '''Convert from index locations in the original
+        data, e.g., surface vertex, to the corresponding
+        index of the outputted ROIs.'''
+
+        labels = self.labels_[locs]
+        return self.labels_to_roi_index(labels)
+
 
 class SurfMaps(BaseEstimator, TransformerMixin):
     '''Extract signals from overlapping labels for surface data.
@@ -554,17 +573,6 @@ class SurfMaps(BaseEstimator, TransformerMixin):
         self.strategy = strategy
         self.mask = mask
         self.vectorize = vectorize
-
-    @property
-    def roi_labels(self):
-        return {i: label for i, label in enumerate(self.non_bkg_unique_)}
-
-    def labels_to_roi_index(self, labels):
-
-        label_map = {label: i for i, label in enumerate(self.non_bkg_unique_)}
-        roi_index = np.array([label_map[l] for l in labels])
-        
-        return roi_index
 
     def fit(self, X, y=None):
         '''Fit this object according
